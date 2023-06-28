51d76e78e1fb16a80c9cd7f8d05fc54828ef6a93
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-06-28 13:10:37 +0200

    [DOCS] MILESTONE 3 aggiunta dei path delle immagini

--- src/components/AppMoviesCard.vue
+++ src/components/AppMoviesCard.vue
@@ -16,6 +16,7 @@ export default {
             <!-- chiamata delle props nei figli -->
         <p>{{ myFilm.title }}</p>
         <p>{{ myFilm.original_title }}</p>
+        <img :src="`https://image.tmdb.org/t/p/w342/${myFilm.poster_path}`" alt="">
         <!-- inserita una img con percorso nei node_modules per prendere le country flag che rappresenteranno la lingua originale -->
         <img class="icons" :src="`../../node_modules/country-flag-icons/1x1/${myFilm.original_language.toUpperCase()}.svg`" alt="">
         <p>{{ myFilm.vote_average }}</p>
--- src/components/AppSeriesTvCard.vue
+++ src/components/AppSeriesTvCard.vue
@@ -14,6 +14,8 @@
         <!-- chiamata delle props nei figli -->
         <p>{{ mySeriesTv.name }}</p>
         <p>{{ mySeriesTv.original_name }}</p>
+        <!-- aggiunta immagini dei film componendo il path con il link, la dimensione e il dato dell'API -->
+        <img :src="`https://image.tmdb.org/t/p/w342/${mySeriesTv.poster_path}`" alt="">
         <!-- inserita una img con percorso nei node_modules per prendere le country flag che rappresenteranno la lingua originale -->
         <img class="icons" :src="`../../node_modules/country-flag-icons/1x1/${mySeriesTv.original_language.toUpperCase()}.svg`" >
         
